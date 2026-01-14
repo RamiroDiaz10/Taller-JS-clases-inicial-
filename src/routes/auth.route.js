4a73ded0a4ae99c8ab464d1d9703380738ea5b68
Ramiro Diaz <jramiroda10@gmail.com>
2026-01-14 16:44:05 -0500

    [+] auth.route.js: se agregar la ruta de auth para register y login

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
+import { loginUser, reNewToken } from "../controllers/auth.controller.js";
+import { createUser } from "../controllers/user.controller.js";
+import authenticationUser from "../middlewares/authentication.middleware.js";
+import authUser from "../middlewares/authUser.middleware.js";
 
 const router = Router();
 
-router.post('/login', )
+router.post('/login', loginUser);
+
+router.post('/register', createUser);
+
+router.get(
+    '/renew-token', 
+    [authenticationUser, authUser],
+    reNewToken
+ );
+ export default router;
