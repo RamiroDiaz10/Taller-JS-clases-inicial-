939398655f64d4f948aa38155700b9c0fc000603
Ramiro Diaz <jramiroda10@gmail.com>
2026-01-14 16:59:42 -0500

    [+] index.js: se agrega las rutas de auth y users

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dbConnection from "./config/mongo.config.js";
+import authRoute from './routes/auth.route.js';
+import userRoute from './routes/user.route.js';
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +9,10 @@ const PORT = 3000;
 dbConnection();
 
 app.use(express.json());
+
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/users', userRoute );
+
 
 app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}/health :()`);
