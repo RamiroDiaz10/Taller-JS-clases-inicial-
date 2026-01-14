2be6670f94079d8bdb73b2415c3d75d1c07bfbd3
Ramiro Diaz <jramiroda10@gmail.com>
2026-01-14 16:38:09 -0500

    [+] auth.controller.js: se agrega el login de user y renewtoken para sesion

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,75 @@
+import { verifyEncriptedPassword } from "../helpers/bcrypt.helper";
+import { generateToken } from "../helpers/jwt.helper";
+import { dbGetUserByEmail } from "../service/user.service.js";
+
+const loginUser = async ( req, res) => {
+
+      
+        const inputData = req.body;
+
+        const userFound = await dbGetUserByEmail(inputData.email);
+
+        if( !userFound ){
+            res.json ({ msg: 'Error: usuario no registrado' });
+        }
+
+        const userMatch = verifyEncriptedPassword(inputData.password, userFound.password);
+
+        if ( !userMatch ){
+            res.json({msg: 'Error: credenciales invalidas'});
+        }
+
+        const payload = {
+            id: userFound._id,
+            name: userFound.name,
+            email: userFound.email,
+            role: userFound.role
+        }
+
+        const token = generateToken(payload);
+
+        const jsonUserFound = userFound.toObject();
+
+        delete jsonUserFound.password;
+
+        res.json ({
+            token,
+            user: jsonUserFound,
+
+        });
+
+}
+
+const reNewToken = async(req, res) => {
+    const payload = req.payload;
+
+    delete payload.iat;
+    delete payload.exp;
+
+    const userFound = await dbGetUserByEmail(payload.email);
+
+        if( !userFound ){
+            res.json ({ msg: 'Error: usuario no registrado' });
+        }
+    
+    const token = generateToken ({
+        id: userFound._id,
+        name: userFound.name,
+        email: userFound.email,
+        role: userFound.role
+    });
+
+    const jsonUserFound = userFound.toObject();
+
+    delete jsonUserFound.password;
+
+    res.json({ 
+        token,  
+        user: jsonUserFound
+    });
+}
+
+export {
+    loginUser,
+    reNewToken
+}
